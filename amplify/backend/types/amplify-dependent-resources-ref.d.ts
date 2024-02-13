0ce2b9e72b73c14e1a370a38b5f220a5cc17bc72
Tharunm06 <tharunm21cb@psnacet.edu.in>
2024-02-13 16:30:24 +0530

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "dishaguard381d948c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  import React from 'react';
+  import { withAuthenticator, Button, Heading, Image, View, Card } from "@aws-amplify/ui-react";
+  import logo from "./logo.svg";
+  import "@aws-amplify/ui-react/styles.css";
+  import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  function App({ signOut }) {
+    return (
+      <View className="App">
+        
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello from V2</h1>
+        </header>
+        <Button onClick={signOut}>Sign Out</Button>
+      </View>
+    );
+  }
+
+  export default withAuthenticator(App);

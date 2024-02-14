cff039d667f01c288b2447cf596a1554fa4819d3
Tharunm06 <tharunm21cb@psnacet.edu.in>
2024-02-14 15:27:13 +0530

    UI update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "@aws-amplify/ui-react/styles.css";
 import './App.css';
 import RegisterVisitor from "./components/listgroup.tsx";
 
+
+
 function App({ signOut }) {
   return (
     <View className="App">
